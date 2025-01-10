16e058dfc32e21a50c00cb016bae0b0717ac6ae0
Alexander J. Lallier <alexanderlallier@aol.com>
2025-01-10 15:29:17 -0500

    Fix static content serving
    Fixed static content serving
    
    * Fix parseUrl error by removing parseUri dep, that didn't seem to be used anyways, in favor of native Node URL parsing capabilities
    * Bumped `@types/node` so Node native URL parsing would have the type definitions needed
    * `tasks.json` was auto updated by VSCode, tested that and made sure it was working

--- out/src/server.js
+++ out/src/server.js
@@ -3,25 +3,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const vscode = require("vscode");
 const path = require("path");
 const send = require("send");
-const parseUrl = require("parseurl");
 class Server {
     constructor(options) {
         const app = require('express')();
         const http = require('http').Server(app);
         const io = require('socket.io')(http);
+        const port = vscode.workspace.getConfiguration("instantmarkdown").get("port");
+        const host = vscode.workspace.getConfiguration("instantmarkdown").get("host");
         app.get('/', function (req, res) {
             res.setHeader('X-Content-Type-Options', 'nosniff');
             res.setHeader('Cache-Control', 'no-store');
-            res.sendfile(path.resolve(__dirname, '..', '..', 'index.html'));
+            res.sendFile(path.resolve(__dirname, '..', '..', 'index.html'));
         });
         app.get('/github-markdown.css', function (req, res) {
-            res.sendfile(path.resolve(__dirname, '..', '..', 'node_modules', 'github-markdown-css', 'github-markdown.css'));
+            res.sendFile(path.resolve(__dirname, '..', '..', 'node_modules', 'github-markdown-css', 'github-markdown.css'));
         });
         app.get('/github-highlight.css', function (req, res) {
-            res.sendfile(path.resolve(__dirname, '..', '..', 'node_modules', 'highlight.js', 'styles', 'github.css'));
+            res.sendFile(path.resolve(__dirname, '..', '..', 'node_modules', 'highlight.js', 'styles', 'github.css'));
         });
         app.get('*', function (req, res) {
-            send(req, parseUrl(req.url).pathname, { root: options.root }).pipe(res);
+            send(req, URL.parse(req.url, `http://${host}`).pathname, { root: options.root }).pipe(res);
         });
         this.sockets = {};
         var nextSocketId = 0;
@@ -30,8 +31,6 @@ class Server {
             this.sockets[socketId] = socket;
             socket.on('close', () => { delete this.sockets[socketId]; });
         });
-        var port = vscode.workspace.getConfiguration("instantmarkdown").get("port");
-        var host = vscode.workspace.getConfiguration("instantmarkdown").get("host");
         http.listen(port, host, function () {
             console.log('listening on *:' + port);
             options.started();
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import * as vscode from 'vscode'
 import * as path from 'path'
 import * as send from 'send'
-import * as parseUrl from 'parseurl'
 
 interface ServerOption {
     root: string;
@@ -16,23 +15,25 @@ export default  class Server {
         const app = require('express')();
         const http = require('http').Server(app);
         const io = require('socket.io')(http);
+        const port = vscode.workspace.getConfiguration("instantmarkdown").get("port");
+        const host = vscode.workspace.getConfiguration("instantmarkdown").get("host");
 
         app.get('/', function(req, res){
             res.setHeader('X-Content-Type-Options', 'nosniff');
             res.setHeader('Cache-Control', 'no-store');
-            res.sendfile(path.resolve(__dirname, '..', '..', 'index.html'));
+            res.sendFile(path.resolve(__dirname, '..', '..', 'index.html'));
         });
 
         app.get('/github-markdown.css', function(req,res) {
-            res.sendfile(path.resolve(__dirname , '..','..','node_modules','github-markdown-css','github-markdown.css'))
+            res.sendFile(path.resolve(__dirname , '..','..','node_modules','github-markdown-css','github-markdown.css'))
         })
 
         app.get('/github-highlight.css', function(req,res) {
-            res.sendfile(path.resolve(__dirname , '..','..','node_modules','highlight.js','styles', 'github.css'))
+            res.sendFile(path.resolve(__dirname , '..','..','node_modules','highlight.js','styles', 'github.css'))
         })
 
         app.get('*', function(req,res) {
-            send(req, parseUrl(req.url).pathname, { root: options.root }).pipe(res);
+            send(req, URL.parse(req.url, `http://${host}`).pathname, { root: options.root }).pipe(res);
         })
 
         this.sockets = {};
@@ -43,8 +44,6 @@ export default  class Server {
             socket.on('close',  () => { delete this.sockets[socketId]; });
         });
 
-        var port = vscode.workspace.getConfiguration("instantmarkdown").get("port");
-        var host = vscode.workspace.getConfiguration("instantmarkdown").get("host");
         http.listen(port, host, function(){
             console.log('listening on *:' + port);
             options.started()
